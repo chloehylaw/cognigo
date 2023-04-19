a25e07003173a2cf47357b2641516fcbbe47908c
z-bruce <87652520+z-bruce@users.noreply.github.com>
2023-04-19 18:59:17 -0400

    initial Tower of Hanoi implementation
    This commit adds initial implementation for a basic Tower of Hanoi exercise:
    - 4 difficulty levels (3 blocks to 6 blocks)
    - basic instructions page
    - back button
    - reset button

---
+++ src/contants/HanoiConstants.js
@@ -0,0 +1,124 @@
+/**
+ * This file contains the initial settings for the varying difficulty levels of the Tower of Hanoi game.
+ * 
+ */
+
+export const easy_mode_init = [
+  {
+    id: "Tile-1",
+    column: 1,
+    row: 4,
+    width: 8
+  },
+  {
+    id: "Tile-2",
+    column: 1,
+    row: 5,
+    width: 10
+  },
+  {
+    id: "Tile-3",
+    column: 1,
+    row: 6,
+    width: 12
+  }
+]
+
+export const medium_mode_init = [
+  {
+    id: "Tile-1",
+    column: 1,
+    row: 3,
+    width: 6
+  },
+  {
+    id: "Tile-2",
+    column: 1,
+    row: 4,
+    width: 8
+  },
+  {
+    id: "Tile-3",
+    column: 1,
+    row: 5,
+    width: 10
+  },
+  {
+    id: "Tile-4",
+    column: 1,
+    row: 6,
+    width: 12
+  }
+]
+
+export const hard_mode_init = [
+  {
+    id: "Tile-1",
+    column: 1,
+    row: 2,
+    width: 4
+  },
+  {
+    id: "Tile-2",
+    column: 1,
+    row: 3,
+    width: 6
+  },
+  {
+    id: "Tile-3",
+    column: 1,
+    row: 4,
+    width: 8
+  },
+  {
+    id: "Tile-4",
+    column: 1,
+    row: 5,
+    width: 10
+  },
+  {
+    id: "Tile-5",
+    column: 1,
+    row: 6,
+    width: 12
+  }
+]
+
+export const very_hard_mode_init = [
+  {
+    id: "Tile-1",
+    column: 1,
+    row: 1,
+    width: 2
+  },
+  {
+    id: "Tile-2",
+    column: 1,
+    row: 2,
+    width: 4
+  },
+  {
+    id: "Tile-3",
+    column: 1,
+    row: 3,
+    width: 6
+  },
+  {
+    id: "Tile-4",
+    column: 1,
+    row: 4,
+    width: 8
+  },
+  {
+    id: "Tile-5",
+    column: 1,
+    row: 5,
+    width: 10
+  },
+  {
+    id: "Tile-6",
+    column: 1,
+    row: 6,
+    width: 12
+  }
+]
