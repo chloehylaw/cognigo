6323ce46b24df9e209d12769e7d4bf363f7bd993
MaximeJoanisse <maxjoanisse101@gmail.com>
2023-04-20 19:30:42 -0400

    Added the About us Page
    added a basic about us page showing what our product is and why we made it., and credit for the website.

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,3 +1,28 @@
+import { Container, Row, Col, Button, Card } from 'react-bootstrap';
+import { useNavigate } from "react-router-dom";
+import brain from '../images/brain.jpg';
+import memory from '../images/memory.jpg';
+import language from '../images/Language.jpg';
+import Visual from '../images/Visual.jpg';
+import attention from '../images/attention.jpg';
+import './progress.css';
+import {BsFill1CircleFill,BsFill2CircleFill, BsFill3CircleFill } from 'react-icons/bs'
+
 export default function AboutUs() {
-    return <h1>About us</h1>
+    return <Container>
+        <h2> What are we?</h2>
+        <p> We are a Company that wishes to enhance the traditional means of cognitive rehabilitation therapy, we do this by using a gamefied approach to better increase your skills in memory, attention, visual spatial reasoning and language. As well, with a customizable interface, you can change the way this whole app looks to better your experience.</p>
+        <h2 style={{marginTop:'1em'}}> Why did we do this?</h2>
+        <p> We felt that patients who had cognitive impariements didn't have the best means to get better given the current system being slow and unmotivating. So we felt with our method, that users could stay motivated and get better from the comfort of their homes.  </p>
+        <Row>
+            <h2 style={{marginTop:'1em', marginBottom:'1em'}}>The engineers behind this app</h2>
+            <Col>Zachary Bruce</Col>
+            <Col>Victoria Castelli</Col>
+            <Col>Maxime Joanisse</Col>
+            <Col>Chloe Hei Yu Law</Col>
+            <Col>Khaled Owaida</Col>
+            <Col>Mauran Pavan</Col>
+
+        </Row>
+    </Container>
 }
