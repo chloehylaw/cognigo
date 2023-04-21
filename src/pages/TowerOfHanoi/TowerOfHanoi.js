4fb6845fe7ddb944d114385726359eb945885f98
z-bruce <87652520+z-bruce@users.noreply.github.com>
2023-04-20 22:45:07 -0400

    [styling] updates to styling of tower of hanoi game elements
    This commit updates the styling for the Tower of Hanoi game to better reflect the project's styling choices.

--- src/pages/TowerOfHanoi/TowerOfHanoi.js
+++ src/pages/TowerOfHanoi/TowerOfHanoi.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import { Button, Col, Row } from "react-bootstrap";
+import { Button, Col, Row, Container } from "react-bootstrap";
 import {easy_mode_init, 
         medium_mode_init, 
         hard_mode_init,
         very_hard_mode_init} from "../../contants/HanoiConstants";
+import '../index.css'
 import '../../assets/css/Hanoi.css'
 
 /**
@@ -25,6 +26,9 @@ function TowerOfHanoi() {
   const [tiles, setTiles] = useState(init);
   const [difficulty, setDifficulty] = useState('');
   const [ready, setReady] = useState(false);
+  const [idealMoves, setIdealMoves] = useState(0)
+
+  let ideal_num_moves = 0;
 
   useEffect(() => {
     initializeDifficulty();
@@ -44,24 +48,34 @@ function TowerOfHanoi() {
   const initializeTiles = () => {
     switch(difficulty) {
       case 'easy':
-        let easy_init = JSON.parse(JSON.stringify(easy_mode_init))
+        let easy_init = JSON.parse(JSON.stringify(easy_mode_init));
         setTiles(easy_init);
+        ideal_num_moves = Math.pow(2, 3) - 1;
+        setIdealMoves(ideal_num_moves);
         break;
       case 'medium':
-        let med_init = JSON.parse(JSON.stringify(medium_mode_init))
+        let med_init = JSON.parse(JSON.stringify(medium_mode_init));
         setTiles(med_init);
+        ideal_num_moves = Math.pow(2, 4) - 1;
+        setIdealMoves(ideal_num_moves);
         break;
       case 'hard':
-        let hard_init = JSON.parse(JSON.stringify(hard_mode_init))
+        let hard_init = JSON.parse(JSON.stringify(hard_mode_init));
         setTiles(hard_init);
+        ideal_num_moves = Math.pow(2, 5) - 1;
+        setIdealMoves(ideal_num_moves);
         break;
       case 'very_hard':
-        let v_hard_init = JSON.parse(JSON.stringify(very_hard_mode_init))
-        setTiles(v_hard_init)
+        let v_hard_init = JSON.parse(JSON.stringify(very_hard_mode_init));
+        setTiles(v_hard_init);
+        ideal_num_moves = Math.pow(2, 6) - 1;
+        setIdealMoves(ideal_num_moves);
         break;
       default:
         let default_init = JSON.parse(JSON.stringify(easy_mode_init))
         setTiles(default_init);
+        ideal_num_moves = Math.pow(2, 3) - 1;
+        setIdealMoves(ideal_num_moves);
         break;
     }
     setMoveCount(0);
@@ -123,112 +137,112 @@ function TowerOfHanoi() {
       <div className="App">
         <Row className="instructions">
           <Col sm={2} md={2} id="back-button-container">
-            <Button variant="secondary" className="back-button" onClick={handleBack}>Back</Button>
+            <Button className="game-button" onClick={handleBack}>Back</Button>
           </Col>
           <Col sm={6} md={8}>
               <p><span className="text-title">Objective:</span> Rebuild the tower in the third column using as few moves as possible</p>
               <p><span className="text-title">Instructions:</span> Move one tile at a time, bigger tiles cannot be placed on top of smaller tiles</p>
           </Col>
           <Col sm={2} md={2} id="reset-button-container">
-            <Button variant="secondary" className="reset-button" onClick={handleReset}>Reset</Button>
+            <Button className="game-button" onClick={handleReset}>Reset</Button>
           </Col>
         </Row>
-        <div className="content">
-          <div
-            className="column-container"
-            id={1}
-            onDragOver={(ev) => ev.preventDefault()}
-            onDrop={handleDrop}
-          >
-            <div className="center-bar" />
-            {column1Tiles
-              .sort((a, b) => a.width - b.width)
-              .map((tile, index) => {
-                const tileCount = column1Tiles.length;
-                const tileStyles = {
-                  width: `${tile.width}em`
-                };
-                tileStyles.marginTop =
-                  index === 0 ? `calc(80vh - ${tileCount * 40 + 20}px)` : "0";
-                return (
-                  <div
-                    {...tile}
-                    className="tile"
-                    draggable
-                    key={`column-1-${tile.id}`}
-                    onDragOver={(ev) => ev.preventDefault()}
-                    onDragStart={handleDrag}
-                    style={tileStyles}
-                  />
-                );
-              })}
+        <Container className="hanoi-game-container">
+          <div className="hanoi-game-window">
+            <div className="hanoi-column" id={1}
+              onDragOver={(ev) => ev.preventDefault()}
+              onDrop={handleDrop}
+            >
+              <div className="center-bar" />
+              {column1Tiles
+                .sort((a, b) => a.width - b.width)
+                .map((tile, index) => {
+                  const tileCount = column1Tiles.length;
+                  const tileStyles = {
+                    width: `${tile.width}em`
+                  };
+                  tileStyles.marginTop =
+                    index === 0 ? `calc(60vh - ${tileCount * 40 + 1}px)` : "0";
+                  return (
+                    <div
+                      {...tile}
+                      className="tile"
+                      draggable
+                      key={`column-1-${tile.id}`}
+                      onDragOver={(ev) => ev.preventDefault()}
+                      onDragStart={handleDrag}
+                      style={tileStyles}
+                    />
+                  );
+                })}
+            </div>
+            <div className="hanoi-column" id={2}
+              onDragOver={(ev) => ev.preventDefault()}
+              onDrop={handleDrop}
+            >
+              <div className="center-bar" />
+              {column2Tiles
+                .sort((a, b) => a.width - b.width)
+                .map((tile, index) => {
+                  const tileCount = column2Tiles.length;
+                  const tileStyles = {
+                    width: `${tile.width}em`
+                  };
+                  tileStyles.marginTop =
+                    index === 0 ? `calc(60vh - ${tileCount * 40 + 1}px)` : "0";
+                  return (
+                    <div
+                      {...tile}
+                      className="tile"
+                      draggable
+                      key={`column-2-${tile.id}`}
+                      onDragOver={(ev) => ev.preventDefault()}
+                      onDragStart={handleDrag}
+                      style={tileStyles}
+                    />
+                  );
+                })}
+            </div>
+            <div className="hanoi-column" id={3}
+              onDragOver={(ev) => ev.preventDefault()}
+              onDrop={handleDrop}
+            >
+              <div className="center-bar" />
+              {column3Tiles
+                .sort((a, b) => a.width - b.width)
+                .map((tile, index) => {
+                  const tileCount = column3Tiles.length;
+                  const tileStyles = {
+                    width: `${tile.width}em`
+                  };
+                  tileStyles.marginTop =
+                    index === 0 ? `calc(60vh - ${tileCount * 40 + 1}px)` : "0";
+                  return (
+                    <div
+                      {...tile}
+                      className="tile"
+                      draggable
+                      key={`column-3-${tile.id}`}
+                      onDragOver={(ev) => ev.preventDefault()}
+                      onDragStart={handleDrag}
+                      style={tileStyles}
+                    />
+                  );
+                })}
+            </div>
           </div>
-          <div
-            className="column-container"
-            id={2}
-            onDragOver={(ev) => ev.preventDefault()}
-            onDrop={handleDrop}
-          >
-            <div className="center-bar" />
-            {column2Tiles
-              .sort((a, b) => a.width - b.width)
-              .map((tile, index) => {
-                const tileCount = column2Tiles.length;
-                const tileStyles = {
-                  width: `${tile.width}em`
-                };
-                tileStyles.marginTop =
-                  index === 0 ? `calc(80vh - ${tileCount * 40 + 20}px)` : "0";
-                return (
-                  <div
-                    {...tile}
-                    className="tile"
-                    draggable
-                    key={`column-2-${tile.id}`}
-                    onDragOver={(ev) => ev.preventDefault()}
-                    onDragStart={handleDrag}
-                    style={tileStyles}
-                  />
-                );
-              })}
-          </div>
-          <div
-            className="column-container"
-            id={3}
-            onDragOver={(ev) => ev.preventDefault()}
-            onDrop={handleDrop}
-          >
-            <div className="center-bar" />
-            {column3Tiles
-              .sort((a, b) => a.width - b.width)
-              .map((tile, index) => {
-                const tileCount = column3Tiles.length;
-                const tileStyles = {
-                  width: `${tile.width}em`
-                };
-                tileStyles.marginTop =
-                  index === 0 ? `calc(80vh - ${tileCount * 40 + 20}px)` : "0";
-                return (
-                  <div
-                    {...tile}
-                    className="tile"
-                    draggable
-                    key={`column-3-${tile.id}`}
-                    onDragOver={(ev) => ev.preventDefault()}
-                    onDragStart={handleDrag}
-                    style={tileStyles}
-                  />
-                );
-              })}
-          </div>
-        </div>
+        </Container>
         {winCondition && (
           <div className="win-message">
             You Win!
-            <div className="win-subtitle">
-              You did it in <span className="win-number">{moveCount}</span>{" "}
-              moves
-            </div>
+            {moveCount == idealMoves ? 
+              <div className="win-subtitle">
+                You solved it in <span className="win-number">{moveCount}</span>moves, the smallest possible number of moves!
+              </div> : 
+              <div className="win-subtitle">
+              You solved it in <span className="win-number">{moveCount}</span> moves!
+              </div>
+            }
           </div>
         )}
         Move count: {moveCount}
